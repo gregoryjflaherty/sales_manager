abaa40d80e246d451a11ae7ab996f5516fa7cf0e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-08 09:14:24 -0400

    Feat: add lead show page

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,9 +5,24 @@ exports.get_landing = function(req, res, next) {
 }
 
 exports.submit_lead = function(req, res, next) {
-    return models.Lead.create({
-        email: red.body.lead_email
+    return models.Leads.create({
+        email: req.body.lead_email
     }).then(lead => {
-        res.redirect('/')
+        res.redirect('/leads')
+    })
+}
+exports.show_leads = function(req, res, next) {
+    models.Leads.findAll().then(leads => {
+        res.render('landing', {title: 'Express', leads: leads});
+    });
+}
+
+exports.show_lead = function(req, res, next) {
+    models.Leads.findOne({
+        where: {
+            id: req.params.lead_id,
+        }
+    }).then(leads => {
+        res.render('lead', {lead: leads});
     });
 }
